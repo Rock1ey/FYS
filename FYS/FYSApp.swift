6560264c20a582d6a70f1c7b906c5dd39dcfa700
Rock1ey <2061218098@qq.com>
2024-11-08 11:28:52 +0800

    Initial Commit

---
+++ FYS/FYSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FYSApp.swift
+//  FYS
+//
+//  Created by admin on 2024/11/8.
+//
+
+import SwiftUI
+
+@main
+struct FYSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
